a8049bd2c56777513f5c29cb09320a2edb650dcc
Asym - Building GitForge <nextbigempire@gmail.com>
2025-10-23 04:22:23 +0100

    Update Button.tsx

--- lib/components/Button.tsx
+++ lib/components/Button.tsx
@@ -18,3 +18,17 @@ const Button: React.FC<ButtonProps> = ({ children, onClick, className }) => {
 };
 
 export default Button;
+
+
+"use client";
+
+export default function Button({ onClick, children }: { onClick: () => void, children: React.ReactNode }) {
+  return (
+    <button
+      onClick={onClick}
+      className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
+    >
+      {children}
+    </button>
+  );
+}
